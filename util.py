0e4498836c86c84d04fb397d4c29a8aab3b23aaa
siro <69783019+massao000@users.noreply.github.com>
2024-01-05 01:57:53 +0900

    機能の追加
    スワップ機能の追加

--- util.py
+++ util.py
@@ -6,15 +6,16 @@ class AspectRatio:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": { "size": ("INT", {"default": 512, "min": 16, "max": MAX_RESOLUTION, "step": 8}),
-                              "aspect": (s.aspect_ratios_label, ),
+                              "aspect_ratios": (s.aspect_ratios_label, ),
                               "standard": (['width', 'height'], ),
+                              "swap_aspect_ratio": ([None, 'swap'], ),
                               "batch_size": ("INT", {"default": 1, "min": 1, "max": 4096})
                             }
                 }
     
     RETURN_TYPES = ("LATENT",)
     FUNCTION = "run"
-    CATEGORY = "aspect_node"
+    CATEGORY = "Aspect Ratios"
 
     def aspect_ratios_file():
         my_path = os.path.dirname(__file__)
@@ -22,7 +23,7 @@ class AspectRatio:
 
         if not os.path.exists(requirements_path):
             aspect_ratios = [
-                "1:1, 1.0 # 1:1 ratio based on minimum dimension\n",
+                "1:1, 1/1 # 1:1 ratio based on minimum dimension\n",
                 "3:2, 3/2 # Set width based on 3:2 ratio to height\n",
                 "4:3, 4/3 # Set width based on 4:3 ratio to height\n",
                 "16:9, 16/9 # Set width based on 16:9 ratio to height",
@@ -51,15 +52,19 @@ class AspectRatio:
                 continue
 
             labels.append(label)
-            values.append(eval(value))
+            values.append(value)
             comments.append(comment)
             
         return labels, values
     
     aspect_ratios_label, aspect_ratios_num = aspect_ratios_file()
 
-    def ar_size(self, is_width, aspect, size):
-        aspect_ratios = (self.aspect_ratios_num[self.aspect_ratios_label.index(aspect)])
+    def ar_size(self, is_width, aspect, size, is_swap):
+        if is_swap:
+            index = (self.aspect_ratios_num[self.aspect_ratios_label.index(aspect)])
+            aspect_ratios = eval('/'.join(index.split('/')[::-1]))
+        else:
+            aspect_ratios = eval(self.aspect_ratios_num[self.aspect_ratios_label.index(aspect)])
         
         if is_width:
             result = size / aspect_ratios
@@ -68,10 +73,11 @@ class AspectRatio:
         
         return int(result)
     
-    def run(self, size, aspect, standard, batch_size):
+    def run(self, size, aspect_ratios, standard, batch_size, swap_aspect_ratio):
         
         is_width = standard == "width"
-        result_dimension = self.ar_size(is_width, aspect, size)
+        is_swap = swap_aspect_ratio == "swap"
+        result_dimension = self.ar_size(is_width, aspect_ratios, size, is_swap)
         
         if is_width:    
             latent = torch.zeros([batch_size, 4, result_dimension // 8, size // 8])
